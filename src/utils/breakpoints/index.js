57759d2990b197cada1980b509f55281cf2d9df5
Kokkiri421 <e.fishev@oy2b.ru>
2022-03-25 01:17:33 +0300

    set breakpoints as module

---
+++ src/utils/breakpoints/index.js
@@ -0,0 +1,11 @@
+const size = {
+  xs: '768px',
+  sm: '992px',
+  xl: '1200px',
+};
+
+export default {
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  xl: `(min-width: ${size.xl})`,
+};
